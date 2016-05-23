12416347fff4107c93f59d12dc0ea4ddecf3c2ac
David Raynes <rayners@gmail.com>
2016-05-23 18:45:59 -0400

    New todo via jquery element

--- public/todo.js
+++ public/todo.js
@@ -1,9 +1,12 @@
 $(function() {
-  $('button#add').click(function() {
+  $('button.btn').click(function(evt) {
     // newTodo is what the user typed into the
     // text box
     var newTodo = $('input').val();
-    $("ul").append("<li>" + newTodo + "</li>");
+    var newTodoEl = $("<li>" + newTodo + "</li>");
+    $("ul").append(newTodoEl);
     $('input').val("");
+
+    evt.preventDefault();
   });
 });
