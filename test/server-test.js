4c3c2cedba3eaf8918e27ebdd743063cf0cbe52e
Ryan Klenk <github@ryanklenk.com>
2017-02-08 20:30:40 -0500

    class updates

--- test/server-test.js
+++ test/server-test.js
@@ -1,5 +1,6 @@
 'use strict';
 
+process.env.NODE_ENV = 'test';
 // code to test
 var server = require('../lib/server');
 
@@ -19,5 +20,28 @@ describe('server', function() {
                 .get('/' + name)
                 .expect(200, 'Hello, ' + name + '!', done);
         });
+    });
+
+    it('should respond with "Hello, Ryan Klenk!"', function() {
+        return request(server)
+            .get('/Ryan?lastName=Klenk')
+            .expect(200,'Hello, Ryan Klenk!')
+    });
+    it('should respond with "Hello, Ryan Klenk! And I understand your inseam is 24 inches. How is the weather down there?" on /Ryan?lastName=Klenk&inseam=24', function(){
+        return request(server)
+            .get('/Ryan?lastName=Klenk&inseam=24')
+            .expect(200,'Hello, Ryan Klenk! And I understand your inseam is 24 inches. How is the weather down there?');
+    });
+
+    it('should respond with "Hello, Ryan Klenk! And I understand your inseam is 38 inches. Wow, you are tall!" on /Ryan?lastName=Klenk&inseam=38', function(){
+        return request(server)
+            .get('/Ryan?lastName=Klenk&inseam=38')
+            .expect(200,'Hello, Ryan Klenk! And I understand your inseam is 38 inches. Wow, you are tall!');
+    });
+
+    it('should respond with "Hello, Ryan Klenk! And I understand your inseam is 30 inches." on /Ryan?lastName=Klenk&inseam=30', function(){
+        return request(server)
+            .get('/Ryan?lastName=Klenk&inseam=30')
+            .expect(200,'Hello, Ryan Klenk! And I understand your inseam is 30 inches.');
     });
 });
