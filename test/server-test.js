ba5038959dc521924af2ebb72d63c034eb94f181
David Raynes <rayners@gmail.com>
2017-05-31 20:06:42 -0400

    In class work

--- test/server-test.js
+++ test/server-test.js
@@ -14,10 +14,44 @@ describe('server', function() {
     });
 
     ['David', 'John', 'Lee'].forEach(function(name) {
-        it('should respond with "Hello, ' + name + '!" on /' + name, function(done) {
-            request(server)
+        it('should respond with "Hello, ' + name + '!" on /' + name, function() {
+            return request(server)
                 .get('/' + name)
-                .expect(200, 'Hello, ' + name + '!', done);
+                .expect(200, 'Hello, ' + name + '!');
+        });
+    });
+
+    it('should handle lastname in the query parameters', function() {
+        return request(server)
+            .get('/David?lastname=Raynes')
+            .expect(200, 'Hello, David Raynes!');
+    });
+
+    describe('inseam', function() {
+        it('should handle the large inseam', function() {
+            return request(server)
+                .get('/David?inseam=36')
+                .expect(200, 'Hello, David! Wow, you are tall!');
+        });
+
+        it('should handle the small inseam', function() {
+            return request(server)
+                .get('/David?inseam=24')
+                .expect(200, 'Hello, David! How is the weather down there?');
+        });
+
+        it('should handle the middle inseam', function() {
+            return request(server)
+                .get('/David?inseam=30')
+                .expect(200, 'Hello, David! And I understand your inseam is 30 inches.')
+        })
+    });
+
+    describe('both query parameters', function() {
+        it('should handle lastname and inseam', function() {
+            return request(server)
+                .get('/David?lastname=Raynes&inseam=36')
+                .expect(200, 'Hello, David Raynes! Wow, you are tall!');
         });
     });
 });
